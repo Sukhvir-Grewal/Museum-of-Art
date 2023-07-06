c810fcbcdcc63bc926e9f5d6f0eaf505da3b9ba2
sukhvir <namedsukhvir@gmail.com>
2023-07-06 12:46:27 -0400

    adding my name for assignment

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,13 @@
+/*********************************************************************************
+*  WEB422 – Assignment 4
+*  I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Sukhvir Singh Student ID: 155312218 Date: 06-July-2023
+*  This assignment was so fun^^
+********************************************************************************/ 
+
 import { Col, Image, Row } from 'react-bootstrap';
 
 const Home = () => {
