f87b65809a4dc95adf67dc34931532680229cb29
sukhvir-singh3 <sukhvir-singh3@gmail.com>
2023-08-08 09:19:44 -0400

    "adding name"

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 /*********************************************************************************
-*  WEB422 – Assignment 5
+*  WEB422 – Assignment 6
 *  I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
 *  No part of this assignment has been copied manually or electronically from any other source
 *  (including web sites) or distributed to other students.
 * 
-*  Name: Sukhvir Singh Student ID: 155312218 Date: 20-July-2023
+*  Name: Sukhvir Singh Student ID: 155312218 Date: 08-APRIL-2023
 *  This assignment was so fun!!!
 ********************************************************************************/ 
 
