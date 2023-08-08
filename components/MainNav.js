8c64d552d9f15e83141265e42838cee455d31a38
sukhvir-singh3 <sukhvir-singh3@gmail.com>
2023-08-08 09:15:06 -0400

    "logout system"

--- components/MainNav.js
+++ components/MainNav.js
@@ -9,21 +9,36 @@ import { useState, useTransition } from 'react';
 import { useRouter } from 'next/router';
 import { searchHistoryAtom } from '@/store';
 import { useAtom } from 'jotai';
+
 import { addToHistory } from '@/lib/userData';
+import { readToken, removeToken } from '@/lib/authenticate';
+
+
+
 
 function MainNav() {
     const Router = useRouter()
     const [inputValue, setInputValue] = useState('')
     const [isExpanded, setIsExpanded] = useState(false)
     const [searchHistory, SetSearchHistory] = useAtom(searchHistoryAtom)
+    const token = readToken();
+
+
+    function logout() {
+        setIsExpanded(false);
+        removeToken();
+        Router.push('/login');
+    }
+
 
     async function handleSubmit(e) {
-        e.preventDefault()
-        SetSearchHistory(await addToHistory(`title=true&q=${inputValue}`)) 
-        Router.push('/artwork?title=true&q=' + inputValue)
-        setInputValue('')
-        setIsExpanded(false)
+        e.preventDefault();
+        SetSearchHistory(await addToHistory(`title=true&q=${inputValue}`));
+        Router.push('/artwork?title=true&q=' + inputValue);
+        setInputValue('');
+        setIsExpanded(false);
     }
+
 
     function handleChangeValue(e) {
         setInputValue(e.target.value)
@@ -32,7 +47,6 @@ function MainNav() {
     function toggleIsExpended(){
         setIsExpanded(!isExpanded)
     } 
-
     function handleNavLink(){
         setIsExpanded(false)
     } 
@@ -48,24 +62,34 @@ function MainNav() {
                             <Link href='/search' passHref legacyBehavior ><Nav.Link active={Router.pathname == '/search'} onClick={handleNavLink}>Advanced Search</Nav.Link></Link>
                         </Nav>
                         &nbsp;
-                        <Form className="d-flex" onSubmit={handleSubmit}>
-                            <Form.Control
-                                type="search"
-                                placeholder="Search"
-                                className="me-2"
-                                aria-label="Search"
-                                value={inputValue}
-                                onChange={handleChangeValue}
-                            />
-                            <Button variant="success">Search</Button>
-                        </Form>
-                        &nbsp;
-                        <Nav>
-                            <NavDropdown title="User Name" id="basic-nav-dropdown">
-                                <Link href='/favourites' passHref legacyBehavior><NavDropdown.Item active={Router.pathname == '/favourites'} onClick={handleNavLink}>Favorites</NavDropdown.Item></Link>
-                                <Link href='/history' passHref legacyBehavior><NavDropdown.Item active={Router.pathname == '/history'} onClick={handleNavLink}>History</NavDropdown.Item></Link>
-                            </NavDropdown>
-                        </Nav>
+                        {token ? (
+                            <>
+                                <Form className="d-flex" onSubmit={handleSubmit}>
+                                    <Form.Control
+                                        type="search"
+                                        placeholder="Search"
+                                        className="me-2"
+                                        aria-label="Search"
+                                        value={inputValue}
+                                        onChange={handleChangeValue}
+                                    />
+                                <Button variant="success">Search</Button>
+                                </Form>
+                                &nbsp;
+                                <Nav>
+                                <NavDropdown title={token.userName} id="basic-nav-dropdown">
+                                    <Link href='/favourites' passHref legacyBehavior><NavDropdown.Item onClick={handleNavLink}>Favorites</NavDropdown.Item></Link>
+                                    <Link href='/history' passHref legacyBehavior><NavDropdown.Item onClick={handleNavLink}>History</NavDropdown.Item></Link>
+                                    <NavDropdown.Item onClick={logout}>Logout</NavDropdown.Item>
+                                </NavDropdown>
+                                </Nav>
+                            </>
+                        ) : (
+                            <Nav>
+                                <Link href='/register' passHref legacyBehavior><Nav.Link active={Router.pathname == '/register'} onClick={handleNavLink}>Register</Nav.Link></Link>
+                                <Link href='/login' passHref legacyBehavior><Nav.Link active={Router.pathname == '/login'} onClick={handleNavLink}>Login</Nav.Link></Link>
+                            </Nav>
+                        )}
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
